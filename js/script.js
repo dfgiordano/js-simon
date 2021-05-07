4b604131d2b2d55990349b8bdf547735d43e0ed5
dfgiordano <federico.giordano90@gmail.com>
2021-05-07 16:00:05 +0200

    mancano controlli iniziali inserimento numeri

--- js/script.js
+++ js/script.js
@@ -10,31 +10,59 @@ function writeNumbers(number) {
   // salvare i valori; dopo che sono stati inseriti tutti e 5, fare il return
   var userNumbers = [];
   for ( i = 0; i < 5; i++) {
+    // var userNumber = parseInt(prompt("Inserisci un numero che ricordi"));
     var userNumber = parseInt(prompt("Inserisci un numero che ricordi"));
+    
     userNumbers.push(userNumber);
+    
   }
   //return dei numeri [1,4,66...]
   return userNumbers;
 }
 /* creo un array di 5 numeri casuali da introdurre poi nell' alert */
 var randomNumbers = []; 
-for ( i = 0; i < 5; i++ ) {
-    var number = getRandomNumber(1, 100);
-    randomNumbers.push(number);
-}
-console.log(randomNumbers);
 
+// for ( i = 0; i < 5; i++ ) {
+//     var number = getRandomNumber(1, 100);
+//     randomNumbers.push(number);
+// }
+// console.log(randomNumbers);
+
+/* e meglio fare un ciclo while in modo che io riesca a controllare--inserisco fino a che non arrivo a length 5-- e poi con includes, controllo che il numero non sia già presente nell'array */
+while (randomNumbers.length < 5) {
+  var number = getRandomNumber(1, 100)
+  if (!randomNumbers.includes(number)) { // costrutto if esegue le parentesi solo quando le condizioni sono VERE in questo cso, col il not davanti deve essere falsa
+    randomNumbers.push(number)
+  }
+}
+// console.log(randomNumbers);
 /* creo alert contenente 5 numeri casuali generati */
 alert("Stanno per arrivare i 5 numeri generati......cerca di ricordarli!")
 alert (randomNumbers);
 
 /* Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). */
+var userNumber;
 var numbersWritten = [];
 setTimeout(function(){
   alert("Vediamo un po'...Quanti dei numeri riesci a ricordare?! ");
   numbersWritten = writeNumbers();
   console.log(numbersWritten);
-}, 30000);
+
+// il confronto va inserito all'interno del timing perchè entrambe le liste devono essere gia generate
+
+var guessed = [];
+
+for (var i = 0; i < randomNumbers.length; i ++) {
+  if (randomNumbers.includes(numbersWritten[i])) {
+    guessed.push(numbersWritten[i]);
+  }
+}
+
+alert("Hai indovinato " + guessed.length + " numeri su 5.");
+console.log("Numeri indovinati: " , guessed);
+
+
+}, 3000);
 
 /* [8,55,33,77,21].includes(77)
 /* Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. 
@@ -44,18 +72,3 @@ var numbersWritten;
 var randomNumbers; 
 */
 // var score = [];
-
-// for (i = 0; i < randomNumbers.length; i++) {
-//   if (randomNumbers.includes(numbersWritten[i]) ) {
-//     score.push(numbersWritten[i]);
-//     console.log(score);
-//   } else {
-  
-//   }
-// }
-
-
-// for ( i = 0 ; i < randomNumbers.length; i++) {
-//   if (numbersWritten[i] == randomNumbers[i])
-//     score.push(i);
-// } ;
